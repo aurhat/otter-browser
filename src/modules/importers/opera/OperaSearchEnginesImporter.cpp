567e51aa9a8b63f202cf39d2a21d6a52330724b5
aurhat <szyk@vivaldi.net>
2018-09-08 12:39:54 +0200

    Leave by default existing search engines during importing

--- src/modules/importers/opera/OperaSearchEnginesImporter.cpp
+++ src/modules/importers/opera/OperaSearchEnginesImporter.cpp
@@ -43,7 +43,7 @@ QWidget* OperaSearchEnginesImporter::createOptionsWidget(QWidget *parent)
 	if (!m_optionsWidget)
 	{
 		m_optionsWidget = new QCheckBox(tr("Remove existing search engines"), parent);
-		m_optionsWidget->setChecked(true);
+		m_optionsWidget->setChecked(false);
 	}
 
 	return m_optionsWidget;
