359f25a6506599fa800598a993137563444d07fe
aurhat <szyk@vivaldi.net>
2018-09-30 11:23:13 +0200

    Show empty branches during using the filter

--- src/ui/ItemViewWidget.cpp
+++ src/ui/ItemViewWidget.cpp
@@ -1066,7 +1066,7 @@ bool ItemViewWidget::applyFilter(const QModelIndex &index, bool parentHasMatch)
 		}
 	}
 
-	setRowHidden(index.row(), index.parent(), (hasFilter ? (!(hasMatch || parentHasMatch) || (isFolder && getRowCount(index) == 0)) : false));
+	setRowHidden(index.row(), index.parent(), (hasFilter ? (!(hasMatch || parentHasMatch)) : false));
 
 	if (isFolder)
 	{
